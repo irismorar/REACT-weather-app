4d184b84a80aec93d99ecaa08f0a4ae5f6f2f6c1
Iris Adina Morar <morariris@gmail.com>
2025-09-30 21:22:17 +0300

    API changed for new data

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,14 @@ import "./App.css";
 export default function App() {
   const [weatherData, setWeatherData] = useState({});
   const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchWeatherData = async () => {
-    setIsLoading(true);
     try {
       const response = await fetch(
-        "https://api.open-meteo.com/v1/forecast?latitude=46.7873443398859&longitude=23.54228778363968&current=temperature_2m,is_day,rain,showers,snowfall,wind_speed_10m"
+        "https://api.open-meteo.com/v1/forecast?latitude=46.75&longitude=23.5&daily=sunrise,sunset,uv_index_max&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,rain,showers,snowfall,snow_depth,cloud_cover,visibility,wind_speed_10m&current=temperature_2m,is_day,rain,showers,snowfall,wind_speed_10m&timezone=Europe%2FBerlin"
       );
       const data = await response.json();
-      if (data) {
-        setError(null);
-        setIsLoading(false);
-      }
       setWeatherData({
         latitude: data.latitude,
         longitude: data.longitude,
@@ -24,9 +19,9 @@ export default function App() {
         currentUnitsData: data.current_units,
       });
       console.log(data);
-    } catch (errorMessage) {
-      console.error(errorMessage);
-      setError("⚠️ Could not load weather data");
+      setIsLoading(false);
+    } catch (errorObject) {
+      setError(`⚠️ Could not load weather data. ${errorObject.message}`);
     } finally {
       setIsLoading(false);
     }
@@ -48,54 +43,56 @@ export default function App() {
     };
   }, [weatherData.currentData?.is_day]);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="error_container">{error}</div>;
+  }
+
   return (
     <main>
       <h1>Thunder Buddy</h1>
-
-      {isLoading && <div>Loading...</div>}
-      {error && <div className="error_container">{error}</div>}
-
-      {!error && !isLoading && (
-        <section>
-          <section className="current-info-container">
-            <div className="temperature">
-              {weatherData.currentData?.temperature_2m +
-                weatherData.currentUnitsData?.temperature_2m}
-            </div>
-            <div className="time">
-              {weatherData.currentData?.time.replace("T", " ")}
-            </div>
-          </section>
-          <section className="additional-info">
-            <h3>Mother Nature’s Tantrums</h3>
-            <div>
-              <p>
-                Wind speed:
-                <span> {weatherData.currentData?.wind_speed_10m}</span>
-                <span> {weatherData.currentUnitsData?.wind_speed_10m}</span>
-              </p>
-              <p>
-                Rain:<span> {weatherData.currentData?.rain}</span>
-                <span> {weatherData.currentUnitsData?.rain}</span>
-              </p>
-              <p>
-                Showers:<span> {weatherData.currentData?.showers}</span>
-                <span> {weatherData.currentUnitsData?.showers}</span>
-              </p>
-              <p>
-                Snowfall:<span> {weatherData.currentData?.snowfall}</span>
-                <span> {weatherData.currentUnitsData?.snowfall}</span>
-              </p>
-            </div>
-          </section>
-          <p>
-            What a fine{" "}
-            {weatherData.currentData?.is_day === 0 ? "night " : "day "}
-            to be lost at <span>{weatherData.latitude} latitude</span>
-            <span> and {weatherData.longitude} longitude</span>
-          </p>
+      <section>
+        <section className="current-info-container">
+          <div className="temperature">
+            {weatherData.currentData.temperature_2m +
+              weatherData.currentUnitsData.temperature_2m}
+          </div>
+          <div className="time">
+            {weatherData.currentData.time.replace("T", " ")}
+          </div>
         </section>
-      )}
+        <section className="additional-info">
+          <h3>Mother Nature’s Tantrums</h3>
+          <div>
+            <p>
+              Latitude: <span>{weatherData.latitude}</span>
+            </p>
+            <p>
+              Longitude: <span>{weatherData.longitude}</span>
+            </p>
+            <p>
+              Wind speed:
+              <span> {weatherData.currentData.wind_speed_10m}</span>
+              <span> {weatherData.currentUnitsData.wind_speed_10m}</span>
+            </p>
+            <p>
+              Rain:<span> {weatherData.currentData.rain}</span>
+              <span> {weatherData.currentUnitsData.rain}</span>
+            </p>
+            <p>
+              Showers:<span> {weatherData.currentData.showers}</span>
+              <span> {weatherData.currentUnitsData.showers}</span>
+            </p>
+            <p>
+              Snowfall:<span> {weatherData.currentData.snowfall}</span>
+              <span> {weatherData.currentUnitsData.snowfall}</span>
+            </p>
+          </div>
+        </section>
+      </section>
     </main>
   );
 }
